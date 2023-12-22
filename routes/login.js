fcb8bc4d9091491dbc50ed061a1fa97dc3aea6d3
Sadanand Miskin <miskinsadanand@gmail.com>
2023-12-23 00:28:52 +0530

    new: public viewable routes

--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ router.get('/' ,(req,res)=>{
 router.post('/' , (req,res)=>{
     const {username , password} = req.body
     
-    if(username === 's' && password === 's') {
+    if(username === process.env.username && password === process.env.pass) {
         req.session.allow = true
       return res.redirect('/')
     }
